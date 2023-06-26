d009cf737753114d01cd051499f8c37a28b6b785
Ivan Proskuryakov <axzhbi5wcm9za3vyewfrb3y@gmail.com>
2023-06-26 14:59:44 +0300

    fix: Cannot find module 'src/lib/Layout/Header' or its corresponding type declarations

---
+++ src/lib/Layout/Header.tsx
@@ -0,0 +1,67 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useContext } from 'react';
+import { AuthContext } from 'src/contexts/Auth';
+import { useNotifications } from 'src/contexts/Notifications';
+import { HeaderWrapper } from 'src/lib/Layout/Wrappers';
+import { useAccount, useDisconnect } from 'wagmi';
+
+
+export default function Header({ }) {
+  const { authStatus } = useContext(AuthContext);
+  const { addNotification } = useNotifications();
+  const router = useRouter();
+  const { disconnect } = useDisconnect()
+  const { address } = useAccount();
+
+  const onLogoutClick = () => {
+    localStorage.clear();
+    router.push('/');
+    addNotification({
+      title: 'See You Around',
+      subtitle: '',
+    });
+    disconnect();
+  }
+
+  const onLoginClick = () => {
+    router.push('/login');
+  }
+
+  return (
+    <HeaderWrapper>
+      <div className="logo">
+        <Link href="/">
+          Communa
+        </Link>
+        <span>alpha</span>
+      </div>
+      <div className="nav">
+        <Link href="/">
+          Browse Jobs
+        </Link>
+        <Link href="/about">
+          About Us
+        </Link>
+        <a href="https://github.com/communa" target="_blank" rel="noreferrer">
+          GitHub
+        </a>
+        {authStatus === 'unauthenticated' && (
+          <button onClick={() => onLoginClick()}>
+            Log In
+          </button>
+        )}
+        {authStatus === 'authenticated' && (
+          <>
+            <Link href={`/user/${address}`}>
+              My Profile
+            </Link>
+            <button onClick={() => onLogoutClick()}>
+              Log Out
+            </button>
+          </>
+        )}
+      </div>
+    </HeaderWrapper>
+  )
+}
